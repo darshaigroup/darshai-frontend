bbad71624c07eee8d75bb629b6b13d4236dfce8f
darshai-tech <rupeshkumardash@darshai.in>
2026-03-24 12:28:11 +0530

    Initial frontend setup of vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+ import './index.css'
+
+
+function App() {
+  
+
+  return (
+   <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-green-600">
+        Tailwind v4 Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
