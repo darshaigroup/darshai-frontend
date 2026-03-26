6c538bd02eaef1e6ec2aadf067fa585f899dd647
sushmitha <sushmithakotian885@gmail.com>
2026-03-26 16:37:13 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,11 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
- import './index.css'
-
+import './index.css';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
-  
-
-  return (
-   <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-5xl font-bold text-green-600">
-        Tailwind v4 Working 
-      </h1>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
